cce7a9577c5554cd7c7e56131f206ec3f2ac8441
mattszeto <ms8288@gmail.com>
2020-06-07 00:37:54 -0700

    edi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          <code>under construction</code>
+          <code>under construction  |  </code>
         </p>
 
         <p>
